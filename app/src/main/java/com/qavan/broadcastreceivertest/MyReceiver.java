c4ad522230c6b9da31624837a9ae4c1521f2dcd9
qavan <pro100qavan@gmail.com>
2020-02-20 12:18:49 +0300

    change

--- app/src/main/java/com/qavan/broadcastreceivertest/MyReceiver.java
+++ app/src/main/java/com/qavan/broadcastreceivertest/MyReceiver.java
@@ -6,11 +6,27 @@ import android.content.Intent;
 import android.widget.Toast;
 
 public class MyReceiver extends BroadcastReceiver {
+    private Intent INTENT;
+    private Context mContext;
     @Override
     public void onReceive(Context context, Intent intent) {
         Toast.makeText(context, "Starting service...", Toast.LENGTH_LONG).show();
-        context.startService(new Intent(context, MyService.class));
-        context.startActivity(new Intent(context, MainActivity.class));
+        INTENT = new Intent(context, MyService.class);
+        mContext = context;
+        try {
+            pvmc();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+//        context.startActivity(new Intent(context, MainActivity.class));
+    }
+
+    public void pvmc() throws InterruptedException {
+        for (int i = 0; i < 10; i++) {
+            mContext.startService(INTENT);
+            Thread.sleep(3000);
+            mContext.stopService(INTENT);
+        }
     }
 }
 
